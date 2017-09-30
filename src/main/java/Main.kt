5e9063cf4a259ccb628db88d09c15a109572f576
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-30 11:28:22 +0200

    Oh No! Dr. Imperative strikes again! We are doomed!

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,5 +1,52 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+
 fun main(args: Array<String>) {
     println(databases)
+}
+
+fun metrics() {
+    var userDatabase = JsonArray<JsonObject>()
+
+    for (database in databases) {
+        userDatabase.addAll(database)
+    }
+
+    var hosts = mutableListOf<String>()
+
+    for (user in userDatabase) {
+        var email = user["email"] as? String
+
+        if (email != null) {
+            var host = email.substringAfter("@")
+
+            if (!hosts.contains(host)) {
+                hosts.add(host)
+            }
+        }
+    }
+}
+
+fun hostInfo(database: JsonArray<JsonObject>, host: String): Pair<Int, Int> {
+    var count = 0
+    var age = 0
+
+    for (user in database) {
+
+        val email = user["email"] as? String
+
+        if (email != null) {
+            val userHost = email.substringAfter("@")
+            val userAge = user["age"] as? Int
+
+            if (userAge != null && userHost == host) {
+                count += 1
+                age += userAge
+            }
+        }
+    }
+
+    return Pair(count, age / count)
 }
